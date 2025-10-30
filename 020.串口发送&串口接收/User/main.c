d950c05462e9d833c5d26925d3e236079e3d9891
KnightWXW <knight960903@gmail.com>
2025-10-30 23:25:41 +0800

    STM32

--- 020.串口发送&串口接收/User/main.c
+++ 020.串口发送&串口接收/User/main.c
@@ -7,8 +7,8 @@ uint8_t RxData; // 接收串口数据的变量
 int main(void)
 {
 	OLED_Init();						  // 初始化 OLED模块
-	
 	Serial_Init(GPIO_Pin_10, GPIO_Pin_9); // 初始化 Serial模块
+	
 	OLED_ShowString(1, 1, "RxData:");
 
 	while (1)
